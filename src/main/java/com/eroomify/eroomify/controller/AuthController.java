df356287766a6938b7fdf0a446696bebd35bf812
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-03 22:24:57 +0530

    Add authorization controller

---
+++ src/main/java/com/eroomify/eroomify/controller/AuthController.java
@@ -0,0 +1,68 @@
+package com.eroomify.eroomify.controller;
+
+import com.eroomify.eroomify.entity.User;
+import com.eroomify.eroomify.exception.UserAlreadyExistsException;
+import com.eroomify.eroomify.request.LoginRequest;
+import com.eroomify.eroomify.response.JwtResponse;
+import com.eroomify.eroomify.security.jwt.JwtUtils;
+import com.eroomify.eroomify.security.user.HotelUserDetails;
+import com.eroomify.eroomify.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public AuthController(UserService userService,
+                          AuthenticationManager authenticationManager,
+                          JwtUtils jwtUtils) {
+        this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/register-user")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        try{
+            userService.registerUser(user);
+            return ResponseEntity.ok("Registration successful!");
+
+        }catch (UserAlreadyExistsException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest request){
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return ResponseEntity.ok(new JwtResponse(
+                userDetails.getId(),
+                userDetails.getEmail(),
+                jwt,
+                roles));
+    }
+}
