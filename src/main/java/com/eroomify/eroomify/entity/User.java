f33c0ff01a5b66064bf4563a14f401ca2b58fce8
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-03 19:46:02 +0530

    Create entity classes

---
+++ src/main/java/com/eroomify/eroomify/entity/User.java
@@ -0,0 +1,29 @@
+package com.eroomify.eroomify.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Entity(name = "user")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST,
+                    CascadeType.MERGE, CascadeType.DETACH})
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
+
+}
