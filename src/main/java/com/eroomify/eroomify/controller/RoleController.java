292bd202955adebf77b178d6f37f2a8f73b29605
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-03 22:35:46 +0530

    Create roles controller

---
+++ src/main/java/com/eroomify/eroomify/controller/RoleController.java
@@ -0,0 +1,60 @@
+package com.eroomify.eroomify.controller;
+
+import com.eroomify.eroomify.entity.Role;
+import com.eroomify.eroomify.entity.User;
+import com.eroomify.eroomify.exception.RoleAlreadyExistException;
+import com.eroomify.eroomify.service.RoleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.FOUND;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/all-roles")
+    public ResponseEntity<List<Role>> getAllRoles(){
+        return new ResponseEntity<>(roleService.getRoles(), FOUND);
+    }
+
+    @PostMapping("/create-new-role")
+    public ResponseEntity<String> createRole(@RequestBody Role theRole){
+        try{
+            roleService.createRole(theRole);
+            return ResponseEntity.ok("New role created successfully!");
+        }catch(RoleAlreadyExistException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+
+        }
+    }
+    @DeleteMapping("/delete/{roleId}")
+    public void deleteRole(@PathVariable("roleId") Long roleId){
+        roleService.deleteRole(roleId);
+    }
+    @PostMapping("/remove-all-users-from-role/{roleId}")
+    public Role removeAllUsersFromRole(@PathVariable("roleId") Long roleId){
+        return roleService.removeAllUsersFromRole(roleId);
+    }
+
+    @PostMapping("/remove-user-from-role")
+    public User removeUserFromRole(
+            @RequestParam("userId") Long userId,
+            @RequestParam("roleId") Long roleId){
+        return roleService.removeUserFromRole(userId, roleId);
+    }
+    @PostMapping("/assign-user-to-role")
+    public User assignUserToRole(
+            @RequestParam("userId") Long userId,
+            @RequestParam("roleId") Long roleId) {
+        return roleService.assignRoleToUser(userId, roleId);
+    }
+}
