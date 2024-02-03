435e7696284086259f256479b78164b8fdbaa570
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-03 19:07:01 +0530

    Initial commit

---
+++ src/main/java/com/eroomify/eroomify/ERoomifyApplication.java
@@ -0,0 +1,13 @@
+package com.eroomify.eroomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERoomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERoomifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eroomify/eroomify/ERoomifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eroomify.eroomify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERoomifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
