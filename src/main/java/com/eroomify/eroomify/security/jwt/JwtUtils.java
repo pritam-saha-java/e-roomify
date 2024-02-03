97d5dba309c80b0b666f6489935cc16deee83898
Pritam Saha <pritamsaha0995@gmail.com>
2024-02-03 22:24:05 +0530

    Add JWT token configuration

---
+++ src/main/java/com/eroomify/eroomify/security/jwt/JwtUtils.java
@@ -0,0 +1,66 @@
+package com.eroomify.eroomify.security.jwt;
+
+import com.eroomify.eroomify.security.user.HotelUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Log4j2
+public class JwtUtils {
+
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
+    private int jwtExpirationMs;
+
+    public String generateJwtTokenForUser(Authentication authentication){
+        HotelUserDetails userPrincipal = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime()+jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+    public String getUserNameFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return true;
+        }catch(MalformedJwtException e){
+            log.error("Invalid jwt token : {} ", e.getMessage());
+        }catch (ExpiredJwtException e){
+            log.error("Expired token : {} ", e.getMessage());
+        }catch (UnsupportedJwtException e){
+            log.error("This token is not supported : {} ", e.getMessage());
+        }catch (IllegalArgumentException e){
+            log.error("No  claims found : {} ", e.getMessage());
+        }
+        return false;
+    }
+
+
+}
